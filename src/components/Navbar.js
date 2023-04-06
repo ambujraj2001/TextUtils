15571d5080c77464b5277ea71d21a11c609f670f
Prateek Niket <prateekniket25@gmail.com>
2023-04-06 17:56:56 +0530

    changed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,13 +8,13 @@ export default function Navbar(props) {
         className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
       >
         <div className="container-fluid">
-            <Link
-              to="/"
-              className="navbar-brand" 
-              style={{ color: "inherit", textDecoration: "inherit" }}
-            >
-              {props.title}
-            </Link>
+          <Link
+            to="/"
+            className="navbar-brand"
+            style={{ color: "inherit", textDecoration: "inherit" }}
+          >
+            {props.title}
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -29,29 +29,28 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                  <Link
-                   className="nav-link active" aria-current="page"
-                    to="/"
-                    style={{ color: "inherit", textDecoration: "inherit" }}
-                  >
-                    {" "}
-                    {props.home}
-                  </Link>
+                <Link
+                  className="nav-link active" aria-current="page"
+                  to="/"
+                  style={{ color: "inherit", textDecoration: "inherit" }}
+                >
+                  {" "}
+                  {props.home}
+                </Link>
               </li>
               <li className="nav-item">
-                  <Link
+                <Link
                   className="nav-link text-active"
-                    to="/About"
-                    style={{ color: "inherit", textDecoration: "inherit" }}
-                  >
-                    {props.about}{" "}
-                  </Link>
+                  to="/About"
+                  style={{ color: "inherit", textDecoration: "inherit" }}
+                >
+                  {props.about}{" "}
+                </Link>
               </li>
             </ul>
             <div
-              className={`form-check form-switch text-${
-                props.mode === "light" ? "dark" : "light"
-              }`}
+              className={`form-check form-switch text-${props.mode === "light" ? "dark" : "light"
+                }`}
             >
               <input
                 className="form-check-input"
@@ -64,7 +63,7 @@ export default function Navbar(props) {
                 className="form-check-label"
                 htmlFor="flexSwitchCheckDefault"
               >
-                Enable Dark Mode
+                Switch to dark mode
               </label>
             </div>
           </div>
